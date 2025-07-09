e8c4c29815ee7d2f19cfea84fcdfa7313ab136b5
Joel Drapper <j.drapper@plane.com>
2025-07-09 10:58:29 +0100

    Add HTML to the raw output

--- quickdraw/compilation_equivalence_cases/raw.rb
+++ quickdraw/compilation_equivalence_cases/raw.rb
@@ -3,7 +3,7 @@
 class Raw < Phlex::HTML
 	def view_template
 		p { "output before" }
-		raw(safe("raw output in the middle"))
+		raw(safe("<h1>raw output in the middle</h1>"))
 		p { "output after" }
 	end
 end
