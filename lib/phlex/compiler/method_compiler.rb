2047ed3c0bee9369e363bdde88d8f763288f7899
Will Cosgrove <will@cosgrove.email>
2025-07-08 21:22:50 -0500

    Add comment helper to compiler

--- lib/phlex/compiler/method_compiler.rb
+++ lib/phlex/compiler/method_compiler.rb
@@ -46,6 +46,8 @@ module Phlex::Compiler
 					return compile_plain_helper(node)
 				elsif fragment_helper?(node)
 					return compile_fragment_helper(node)
+				elsif comment_helper?(node)
+					return compile_comment_helper(node)
 				end
 			end
 
@@ -194,6 +196,14 @@ module Phlex::Compiler
 			)
 		end
 
+		def compile_comment_helper(node)
+			[
+				buffer("<!-- "),
+				visit_phlex_block(node.block),
+				buffer(" -->"),
+			]
+		end
+
 		private def ensure_new_line
 			proc(&:ensure_new_line)
 		end
@@ -291,6 +301,10 @@ module Phlex::Compiler
 
 		private def fragment_helper?(node)
 			node.name == :fragment && own_method_without_scope?(node)
+		end
+
+		private def comment_helper?(node)
+			node.name == :comment && own_method_without_scope?(node)
 		end
 
 		ALLOWED_OWNERS = [Phlex::SGML, Phlex::HTML, Phlex::SVG]
