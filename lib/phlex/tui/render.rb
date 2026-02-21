8bc0e594478b0c7024bb33891cf20fa97af90dd7
Joel Drapper <j.drapper@plane.com>
2026-02-21 23:15:47 +0000

    Update render.rb

--- lib/phlex/tui/render.rb
+++ lib/phlex/tui/render.rb
@@ -79,9 +79,9 @@ class Phlex::TUI::Render
 		traverse_bottom_up(@root, :fit_width)
 		traverse_top_down(@root, :grow_width)
 		traverse_top_down(@root, :normalize_table_widths)
-		traverse_top_down(@root, :wrap_text)
 		traverse_bottom_up(@root, :fit_width)
 		traverse_top_down(@root, :grow_width)
+		traverse_top_down(@root, :wrap_text)
 		traverse_bottom_up(@root, :fit_height)
 		traverse_top_down(@root, :grow_height)
 		traverse_top_down(@root, :normalize_table_heights)
@@ -170,10 +170,10 @@ class Phlex::TUI::Render
 	private def children_bounds(node)
 		if Phlex::TUI::Box === node && node.overflow == :none
 			{
-				row: node.row + node.border_top_width,
-				col: node.col + node.border_left_width,
-				width: [node.width - node.border_horizontal, 0].max,
-				height: [node.height - node.border_vertical, 0].max,
+					row: node.row + node.border_top_width,
+					col: node.col + node.border_left_width,
+					width: [node.width - node.border_horizontal, 0].max,
+					height: [node.height - node.border_vertical, 0].max,
 			}
 		else
 			node_bounds(node)
